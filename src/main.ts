c58f76b75adac08c325d7199168fc0f229b72e0b
TB <thrishbg15@gmail.com>
2025-12-01 23:01:19 +0000

    Remove node_modules from Git tracking

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ console.log('JobGenie is running!');
 
 const app = document.getElementById('app');
 if (app) {
-  app.innerHTML = '<p>Welcome to JobGenie</p>';
+	app.innerHTML = '<p>Welcome to JobGenie</p>';
 }
